3923e24c0d3f6814928f82a562d51b9681fecb08
Aniruddha8 <aniruddhajadhav88888@gmail.com>
2020-09-02 10:28:24 +0530

    Updates

---
+++ precache-manifest.5566e3b08b097162795066c5db597049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c761b49a14ba5bb33567040c24e3ef2a",
+    "url": "/product-launch-react/index.html"
+  },
+  {
+    "revision": "21b125681a98232a1525",
+    "url": "/product-launch-react/static/css/main.c98f8d08.chunk.css"
+  },
+  {
+    "revision": "b3747021e03744b5e0d4",
+    "url": "/product-launch-react/static/js/2.89ec196d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/product-launch-react/static/js/2.89ec196d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b125681a98232a1525",
+    "url": "/product-launch-react/static/js/main.022e71c7.chunk.js"
+  },
+  {
+    "revision": "bdd44d79e6e419a08942",
+    "url": "/product-launch-react/static/js/runtime-main.d3826ac4.js"
+  }
+]);
